37b4b18432c18da02933f07dbc0861fb2ad02daa
Eduardo Corso <educorsoc03@gmail.com>
2024-11-20 11:31:27 -0300

    Add files via upload

---
+++ Educ5/random questionsc5/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'apenas um show',
+    'Qual o verdadeiro nome do personagem saltitão?',
+    'Gregory Nicholas Walks Johnson'
+)
+
+criaCartao(
+    'Pokemon',
+    'Qual o é  pokemon de número 555?',
+    'Darmanitan'
+)
+
+criaCartao(
+    'Yu-gi-oh',
+    'Qual a carta mais difícil de ser invocada?',
+    'Exodia'
+)
+
+criaCartao(
+    'Free Fire',
+    'Qual arma da classe submetralhadora tem ou teve o maior dano registrado?',
+    'CG15'
+)
